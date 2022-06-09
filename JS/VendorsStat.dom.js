67779a494df5538bc310563b9d3b2598b9b64c3f
cyanda1007 <siyaphiwa196@gmail.com>
2022-06-09 18:09:01 +0200

    added modified files

--- JS/VendorsStat.dom.js
+++ JS/VendorsStat.dom.js
@@ -1,134 +1,134 @@
-// var domFunction = theProductStats()
+// // var domFunction = theProductStats()
+// // const enterButtonElem = document.querySelector(".calculate")
+
+// // const CalculateButtonElem = document.querySelector(".calculateButton")
+
+
+// //  //update settings
+
+// //  enterButtonElem.addEventListener('click', function(){
+// //     SoldEggsElem = Number(document.querySelector(".eggsold").value);
+// //     SoldFruitsElem = Number(document.querySelector(".fruitsold").value);                      
+// //     SoldVeggiesElem = Number(document.querySelector(".vegsold").value);  
+    
+    
+     
+// //     // // set input data
+// //         domFunction.setSoldEggs(Number(SoldEggsElem.value));
+// //         domFunction.setSoldFruits(Number(SoldFruitsElem.value));
+// //         domFunction.setSoldVeggies(Number(SoldVeggiesElem.value));
+     
+
+// //         const OverallProfitElem = document.querySelector(".overallprofit");
+
+   
+   
+
+// // })
+// // CalculateButtonElem.addEventListener('click', function() {
+
+// //     var goodsRadioElem = document.querySelector("input[name='Goods']:checked").value; 
+// //     const eggsProfitE = document.querySelector(".eggProfit")
+// //     const FruitsProfitElem = document.querySelector(".fruitprofit");
+// //     const  VeggiesProfitElem = document.querySelector(".vegprofit");
+// //     const OverallProfitElem = document.querySelector(".overallprofit");
+
+// //     if(goodsRadioElem=== "eggs") {
+// //              domFunction.Eggs()
+// //              eggsProfitE.va= domFunction.getEggsProfit();
+         
+                
+// //             }
+// //             if(goodsRadioElem === "fruits"){
+// //                  domFunction.Fruits()  
+// //                 FruitsProfitElem.innerHTML = domFunction.getFruitsProfit();
+// //                 console.log(FruitsProfitElem);
+// //             }
+// //             if(goodsRadioElem === "veggies"){
+                
+// //                 domFunction.Veggies()  
+// //                 VeggiesProfitElem.innerHTML = domFunction.getVeggiesProfit();
+// //             }
+        
+        
+// //             OverallProfitElem.innerHTML = domFunction.getOverallProfit();
+    
+
+
+   
+// // })
+
+// let  domFunction = theProductStats()
+
 // const enterButtonElem = document.querySelector(".calculate")
 
 // const CalculateButtonElem = document.querySelector(".calculateButton")
+// // alert(enterButtonElem)
 
 
-//  //update settings
 
-//  enterButtonElem.addEventListener('click', function(){
-//     SoldEggsElem = Number(document.querySelector(".eggsold").value);
-//     SoldFruitsElem = Number(document.querySelector(".fruitsold").value);                      
-//     SoldVeggiesElem = Number(document.querySelector(".vegsold").value);  
-    
-    
-     
-//     // // set input data
-//         domFunction.setSoldEggs(Number(SoldEggsElem.value));
-//         domFunction.setSoldFruits(Number(SoldFruitsElem.value));
-//         domFunction.setSoldVeggies(Number(SoldVeggiesElem.value));
-     
 
-//         const OverallProfitElem = document.querySelector(".overallprofit");
+// enterButtonElem.addEventListener("click", function () {
 
+
+
+// // refence input data
+// let  SoldEggsElem = document.querySelector(".eggsold").value;
+// //  alert(SoldEggsElem)
+// console.log(parseInt(SoldEggsElem))
+// let SoldFruitsElem =document.querySelector(".fruitsold").value;  
+                     
+// let SoldVeggiesElem = document.querySelector(".vegsold").value;  
+
+
+ 
+// // set input data
+//     domFunction.setSoldEggs(Number(SoldEggsElem.value));
+//     domFunction.setSoldFruits(Number(SoldFruitsElem.value));
+//     domFunction.setSoldVeggies(Number(SoldVeggiesElem.value));
+ 
+
+// //     EggsProfitElem= document.querySelector(".eggProfit");
+// //    FruitsProfitElem = document.querySelector(".fruitprofit");
+// //    VeggiesProfitElem = document.querySelector(".vegprofit");
+// //    OverallProfitElem = document.querySelector(".overallprofit");
    
-   
-
 // })
-// CalculateButtonElem.addEventListener('click', function() {
+// // reference calculated data
 
-//     var goodsRadioElem = document.querySelector("input[name='Goods']:checked").value; 
-//     const eggsProfitE = document.querySelector(".eggProfit")
+
+//  const  eggsProfitE = document.querySelector(".eggProfit")
 //     const FruitsProfitElem = document.querySelector(".fruitprofit");
 //     const  VeggiesProfitElem = document.querySelector(".vegprofit");
-//     const OverallProfitElem = document.querySelector(".overallprofit");
-
-//     if(goodsRadioElem=== "eggs") {
-//              domFunction.Eggs()
-//              eggsProfitE.va= domFunction.getEggsProfit();
-         
-                
-//             }
-//             if(goodsRadioElem === "fruits"){
-//                  domFunction.Fruits()  
-//                 FruitsProfitElem.innerHTML = domFunction.getFruitsProfit();
-//                 console.log(FruitsProfitElem);
-//             }
-//             if(goodsRadioElem === "veggies"){
-                
-//                 domFunction.Veggies()  
-//                 VeggiesProfitElem.innerHTML = domFunction.getVeggiesProfit();
-//             }
-        
-        
-//             OverallProfitElem.innerHTML = domFunction.getOverallProfit();
-    
+//    const OverallProfitElem = document.querySelector(".overallprofit");
 
 
-   
-// })
-
-let  domFunction = theProductStats()
-
-const enterButtonElem = document.querySelector(".calculate")
-
-const CalculateButtonElem = document.querySelector(".calculateButton")
-// alert(enterButtonElem)
-
-
-
-
-enterButtonElem.addEventListener("click", function () {
-
-
-
-// refence input data
-let  SoldEggsElem = document.querySelector(".eggsold").value;
-//  alert(SoldEggsElem)
-console.log(parseInt(SoldEggsElem))
-let SoldFruitsElem =document.querySelector(".fruitsold").value;  
-                     
-let SoldVeggiesElem = document.querySelector(".vegsold").value;  
-
-
- 
-// set input data
-    domFunction.setSoldEggs(Number(SoldEggsElem.value));
-    domFunction.setSoldFruits(Number(SoldFruitsElem.value));
-    domFunction.setSoldVeggies(Number(SoldVeggiesElem.value));
- 
-
-//     EggsProfitElem= document.querySelector(".eggProfit");
-//    FruitsProfitElem = document.querySelector(".fruitprofit");
-//    VeggiesProfitElem = document.querySelector(".vegprofit");
-//    OverallProfitElem = document.querySelector(".overallprofit");
-   
-})
-// reference calculated data
-
-
- const  eggsProfitE = document.querySelector(".eggProfit")
-    const FruitsProfitElem = document.querySelector(".fruitprofit");
-    const  VeggiesProfitElem = document.querySelector(".vegprofit");
-   const OverallProfitElem = document.querySelector(".overallprofit");
-
-
-CalculateButtonElem.addEventListener("click", function () {
+// CalculateButtonElem.addEventListener("click", function () {
 
     
    
-    var goodsRadioElem = document.querySelector("input[name='Goods']:checked").value; 
+//     var goodsRadioElem = document.querySelector("input[name='Goods']:checked").value; 
    
 
    
       
-    if(goodsRadioElem=== "eggs"){
-     domFunction.Eggs(Number(goodsRadioElem)); 
-     eggsProfitE.value= domFunction.getEggsProfit();
-     console.log(eggsProfitE);
-        // alert(EggsProfitElem);
-    }
-    if(goodsRadioElem === "fruits"){
+//     if(goodsRadioElem=== "eggs"){
+//      domFunction.Eggs(Number(goodsRadioElem)); 
+//      eggsProfitE.value= domFunction.getEggsProfit();
+//      console.log(eggsProfitE);
+//         // alert(EggsProfitElem);
+//     }
+//     if(goodsRadioElem === "fruits"){
         
-        domFunction.Fruits()  
-        FruitsProfitElem.value = domFunction.getFruitsProfit();
-    }
-    if(goodsRadioElem === "veggies"){
+//         domFunction.Fruits()  
+//         FruitsProfitElem.value = domFunction.getFruitsProfit();
+//     }
+//     if(goodsRadioElem === "veggies"){
         
-        domFunction.Veggies()  
-        VeggiesProfitElem.value = domFunction.getVeggiesProfit();
-    }
+//         domFunction.Veggies()  
+//         VeggiesProfitElem.value = domFunction.getVeggiesProfit();
+//     }
 
 
-    OverallProfitElem.innerHTML = domFunction.getOverallProfit();
-})
+//     OverallProfitElem.innerHTML = domFunction.getOverallProfit();
+// })
