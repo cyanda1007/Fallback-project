9287761d95671907870387953a278ce600d114c7
cyanda1007 <siyaphiwa196@gmail.com>
2022-06-09 18:28:24 +0200

    added modified

--- JS/test.dom.js
+++ JS/test.dom.js
@@ -2,18 +2,26 @@ const domFunction = Calculate();
 
 const EggsProfit = document.querySelector(".eggProfit")
 const fruitsProfit = document.querySelector(".fruitprofit")
+const veggiesProfit = document.querySelector(".vegprofit")
 
 const  SoldEggsElem = document.querySelector(".eggsold");
 const  SoldfruitsElem = document.querySelector(".fruitsold");
+const  SoldVeggiesElem = document.querySelector(".vegsold");
+
+const totalProfit = document.querySelector(".overallprofit");
+
 const CalculateButtonElem = document.querySelector(".calculateButton")
 const percentElem = document.querySelector(".eggPercent")
 const fruitpercentElem = document.querySelector(".fruitPercent")
+const veggiePercentElem = document.querySelector(".vegPercent")
 const radioEgg = document.querySelector(".radioEggs");
 const radioFruit = document.querySelector(".radioFruit");
+const radioVeggie = document.querySelector(".radioVeggie")
 const enterButtonElem = document.querySelector(".calculate")
 
 let product = "";
 let fruitProduct = "";
+let veggieProduct = "";
 
 radioEgg.addEventListener("click", ()=>{
     product ='eggs';
@@ -23,17 +31,44 @@ radioFruit.addEventListener("click", ()=>{
     product ='fruits';
 })
 
+radioVeggie.addEventListener("click", ()=>{
+    product = 'veggies';
+})
+let profitForEgg = 0;
+let profitForFruit = 0;
+let profitForVeggie = 0;
+
 CalculateButtonElem.addEventListener('click', ()=>{
    
 
     if(product === 'eggs'){
      let  profitArray =   domFunction.eggProfit(SoldEggsElem.value)
+     let profitNum = Number(profitArray[0].replace("R", ""))
 EggsProfit.value = profitArray[0]
+profitForEgg = profitNum
 percentElem.value = profitArray[1]  
+ 
     }
 
 
 
 
-  
+    if(product === 'fruits'){
+  let  fruitprofitArray = domFunction.fruitProfit(SoldfruitsElem.value)
+  let profitFruitNum = Number(fruitprofitArray[0].replace("R", ""))
+  fruitsProfit.value = fruitprofitArray[0]
+  profitForFruit = profitFruitNum
+  fruitpercentElem.value = fruitprofitArray[1] 
+    } 
+
+
+    if(product === 'veggies'){
+        let veggieprofitArray = domFunction.veggieProfit(SoldVeggiesElem.value)
+        let VeggieProfitNum = Number( veggieprofitArray[0].replace("R", ""))
+        veggiesProfit.value = veggieprofitArray[0]
+        profitForVeggie = VeggieProfitNum
+        veggiePercentElem.value = veggieprofitArray[1]
+    }
+    totalProfit.value = profitForEgg + profitForFruit + profitForVeggie
+
 })
--- test.js
+++ test.js
@@ -1,24 +1,53 @@
 function Calculate(){
     let eggTray = 50;
     let eggAmount = 30;
-   
-let profit
+    let fruitBunch = 40;
+    let fruitAmount = 30;
+    let veggieBunch = 65;
+    let veggieAmount = 35;
+  
+
  function eggProfit(eggsold){
  
 let pricePerEgg = 3;
-profit = (pricePerEgg - eggTray/eggAmount) * eggsold
+let profit = (pricePerEgg - eggTray/eggAmount) * eggsold
 
 let percentage = ((eggTray/eggAmount)/pricePerEgg) * 100
-
-return  [`R${profit}`, percentage.toFixed(2)]
+let eggs =  "R" + profit.toFixed(2)
+return [eggs, percentage.toFixed(2)]
 
 }
+
+function fruitProfit(fruitsold){
+ 
+    let pricePerfruit = 2;
+   let fruitprofit = (pricePerfruit - fruitBunch/fruitAmount) * fruitsold
+   
+   let percentages = ((fruitBunch/fruitAmount)/pricePerfruit) * 100
+   let fruit =  "R" + fruitprofit.toFixed(2)
+   return [fruit, percentages.toFixed(2)]
+   
+   }
+   
+   function veggieProfit(veggiesold){
+
+       let pricePerVeggie = 4;
+       let veggiesprofit = (pricePerVeggie - veggieBunch/veggieAmount) * veggiesold
+
+       let percentageS = ((veggieBunch/veggieAmount)/pricePerVeggie) * 100
+    let veg = "R"+ veggiesprofit.toFixed(2)
+       return [veg, percentageS.toFixed(2)]
+
+   }
 
 
 
 
 return{
 eggProfit,
+fruitProfit,
+veggieProfit,
+
 
 }
 
